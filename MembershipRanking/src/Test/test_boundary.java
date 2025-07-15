f6dc89504d401edffb597c2669037a3b19322b2e
Supawit Saktavin <supawit.sa@kkumail.com>
2025-07-15 20:49:55 +0700

    create package test

---
+++ MembershipRanking/src/Test/test_boundary.java
@@ -0,0 +1,14 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test_boundary {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
