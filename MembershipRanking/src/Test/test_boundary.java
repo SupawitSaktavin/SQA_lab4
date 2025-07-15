ccc814f43f98ad08c3aee94d4326e51db1ad53e0
Supawit Saktavin <supawit.sa@kkumail.com>
2025-07-15 21:22:54 +0700

    update test case

--- MembershipRanking/src/Test/test_boundary.java
+++ MembershipRanking/src/Test/test_boundary.java
@@ -2,7 +2,6 @@ package Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -15,19 +14,19 @@ class test_boundary {
 	@CsvSource({
 	"0,		4, 500, Standard",
 	"1,		4, 500, Standard",
-	"50000,	4, 500, Standard",
-	"99999,	4, 500, Standard",
-	"100000,4, 500, Standard",
+	"50000,	4, 500, Gold",
+	"99999,	4, 500, Gold",
+	"100000,4, 500, Gold",
 	
 	"50000, 0, 500, Standard",
-	"50000, 1, 500, Standard",
+	"50000, 1, 500, Silver",
+	"50000, 6, 500, Standard",
 	"50000, 7, 500, Standard",
-	"50000, 8, 500, Standard",
 	
 	"50000, 4, 0, Standard",
 	"50000, 4, 1, Standard",
-	"50000, 4, 999, Standard",
-	"50000, 4, 1000, Standard",
+	"50000, 4, 999, Gold",
+	"50000, 4, 1000, Gold"
 	})
 	void testBoundary(int purchaseTotal, int frequency, int pointCollected, String result) {
 		assertEquals(result, test.CalculateMembershipRank(purchaseTotal, frequency, pointCollected));
